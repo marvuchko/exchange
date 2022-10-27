f9b607620816d1d68bb4f50e0fbc87af248c3c31
Marko Vučković <marko.vuckovic@primeholding.com>
2022-10-27 12:19:54 +0200

    refactor: 💡 [WIP] Cacheable repository refactored

--- core/src/main/java/pro/programista/domain/repository/DomainRepository.java
+++ core/src/main/java/pro/programista/domain/repository/DomainRepository.java
@@ -1,17 +1,10 @@
 package pro.programista.domain.repository;
 
-import static java.util.Objects.nonNull;
-
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import org.springframework.util.CollectionUtils;
 import pro.programista.domain.entity.DomainEntity;
 import pro.programista.domain.primitives.PageResult;
-import pro.programista.domain.service.Cache;
 
 public interface DomainRepository<K extends Serializable, T extends DomainEntity<K>> {
 
@@ -25,54 +18,4 @@ public interface DomainRepository<K extends Serializable, T extends DomainEntity
 
   void delete(K id);
 
-  @RequiredArgsConstructor
-  @FieldDefaults(level = AccessLevel.PROTECTED, makeFinal = true)
-  class CacheableRepository<
-      K extends Serializable,
-      T extends DomainEntity<K>> implements DomainRepository<K, T> {
-
-    Cache<K, T> cache;
-    DomainRepository<K, T> delegate;
-
-    @Override
-    public Optional<T> findById(K id) {
-      T data = cache.get(id);
-      if (nonNull(data)) {
-        return Optional.of(data);
-      }
-      var entityOptional = delegate.findById(id);
-      entityOptional.ifPresent(entity -> cache.put(entity.getId(), entity));
-      return entityOptional;
-    }
-
-    @Override
-    public List<T> findByIds(List<K> ids) {
-      List<T> data = cache.get(ids);
-      if (!CollectionUtils.isEmpty(data)) {
-        return data;
-      }
-      List<T> entities = delegate.findByIds(ids);
-      entities.forEach(entity -> cache.put(entity.getId(), entity));
-      return entities;
-    }
-
-    @Override
-    public PageResult<T> findPage(long page, long size) {
-      return delegate.findPage(page, size);
-    }
-
-    @Override
-    public T createOrUpdate(T data) {
-      var entity = delegate.createOrUpdate(data);
-      cache.put(entity.getId(), entity);
-      return entity;
-    }
-
-    @Override
-    public void delete(K id) {
-      delegate.delete(id);
-      cache.remove(id);
-    }
-
-  }
 }
--- user/src/main/java/pro/programista/infrastructure/persistence/user/UserCacheableJpaRepository.java
+++ user/src/main/java/pro/programista/infrastructure/persistence/user/UserCacheableJpaRepository.java
@@ -2,12 +2,13 @@ package pro.programista.infrastructure.persistence.user;
 
 import java.util.UUID;
 import pro.programista.domain.entity.User;
+import pro.programista.domain.repository.CacheableRepository;
 import pro.programista.domain.repository.DomainRepository;
 import pro.programista.domain.repository.UserRepository;
 import pro.programista.domain.service.Cache;
 
 public class UserCacheableJpaRepository
-    extends DomainRepository.CacheableRepository<UUID, User>
+    extends CacheableRepository<UUID, User>
     implements UserRepository {
 
   public UserCacheableJpaRepository(
